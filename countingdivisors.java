8416740b38ce4afe81de914e4ff66d3dad8d588c
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-05 15:42:59 +0530

    Add files via upload

---
+++ countingdivisors.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class countingdivisors{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int j=0;j<t;j++){
+            int n=sc.nextInt();
+            int c=0;
+            for(int i=1;i<=n;i++){
+                if(n%i==0){
+                    c+=1;
+                }
+            }
+            System.out.println(c);
+        }
+    }
+}
