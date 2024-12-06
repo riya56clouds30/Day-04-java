e9d08f2fdc722411a9f2c23429afb83f8051abf6
riya56clouds30 <priyapriya07264@gmail.com>
2024-12-06 09:36:21 +0530

    Add files via upload

---
+++ increamentanddecreament.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class increamentanddecreament{
+    public static void main(String args[])
+    {
+        int n=5;
+        System.out.println(n++);
+        int a=4;
+        System.out.println(++a);
+        int b=6;
+        System.out.println(--b);
+        int a1=56;
+        System.out.println(a1--);
+    }
+}
